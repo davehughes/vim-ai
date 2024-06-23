450efee1e35195fc3d09594fcbb72c387b6311cb
David Hughes <d@vidhughes.com>
2024-06-22 23:52:43 -0700

    Consolidate code

--- py/chat.py
+++ py/chat.py
@@ -1,59 +1,8 @@
 import vim
 
-import traceback
-
 
 # import utils
 plugin_root = vim.eval("s:plugin_root")
 vim.command(f"py3file {plugin_root}/py/utils.py")
-vim.command(f"py3file {plugin_root}/py/backends.py")
 
-# chat_options = parse_chat_header_options()
-# options = {**config_options, **chat_options}
-# openai_options = make_openai_options(options)
-# http_options = make_http_options(options)
-
-
-def get_messages():
-    # initial_prompt = '\n'.join(options.get('initial_prompt', []))
-    # initial_messages = parse_chat_messages(initial_prompt)
-
-    chat_content = vim.eval('trim(join(getline(1, "$"), "\n"))')
-    chat_messages = parse_chat_messages(chat_content)
-    # messages = initial_messages + chat_messages
-    return chat_messages
-
-
-prompt, role = parse_prompt_and_role(vim.eval("l:prompt"))
-initialize_chat_window()
-messages = get_messages()
-
-try:
-    if messages[-1]["content"].strip():
-        is_selection = vim.eval("l:is_selection")
-        backend = LoremIpsumBackend()
-
-        vim.command("normal! Go\n<<< assistant\n\n")
-        vim.command("redraw")
-
-        print('Answering...1')
-        vim.command("redraw")
-
-        printDebug("[chat] messages: {}", messages)
-        response = backend.chat(messages)
-
-        def map_chunk(resp):
-            printDebug("[chat] response: {}", resp)
-            return resp
-            # return resp['choices'][0]['delta'].get('content', '')
-
-        # response = (i for i in ["foo", "\nbar", "\nbaz"])
-        text_chunks = map(map_chunk, response)
-        render_text_chunks(text_chunks, is_selection)
-
-        vim.command("normal! a\n\n>>> user\n\n")
-        vim.command("redraw")
-        clear_echo_message()
-except BaseException as error:
-    handle_completion_error(error)
-    printDebug("[chat] error: {}", traceback.format_exc())
+do_chat()
--- py/utils.py
+++ py/utils.py
@@ -4,11 +4,13 @@ import glob
 import os
 import socket
 import re
-from urllib.error import URLError
-from urllib.error import HTTPError
+from urllib.error import URLError, HTTPError
 
-_plugin_root = vim.eval("s:plugin_root")
-vim.command(f"py3file {_plugin_root}/py/config.py")
+import sys
+import traceback
+
+import urllib
+import json
 
 
 class KnownError(Exception):
@@ -134,40 +136,6 @@ def parse_chat_messages(chat_content, pwd=config.pwd):
     return messages
 
 
-def parse_chat_header_options():
-    lines = vim.eval('getline(1, "$")')
-    return _parse_chat_header_options(lines)
-
-
-def _parse_chat_header_options(text):
-    text_lines = []
-    if isinstance(text, str):
-        text_lines = text.splitlines()
-    elif isinstance(text, list):
-        text_lines = text
-
-    try:
-        options = {}
-        contains_chat_options = '[chat-options]' in text_lines
-        if contains_chat_options:
-            # parse options that are defined in the chat header
-            options_index = text_lines.index('[chat-options]')
-            for line in text_lines[options_index + 1:]:
-                if line.startswith('#'):
-                    # ignore comments
-                    continue
-                if line == '':
-                    # stop at the end of the region
-                    break
-                (key, value) = line.strip().split('=')
-                if key == 'initial_prompt':
-                    value = value.split('\\n')
-                options[key] = value
-        return options
-    except:
-        raise Exception("Invalid [chat-options]")
-
-
 def vim_break_undo_sequence():
     # breaks undo sequence (https://vi.stackexchange.com/a/29087)
     vim.command("let &ul=&ul")
@@ -256,91 +224,107 @@ def initialize_chat_window(prompt=None):
         vim.command("redraw")
 
 
-# OPENAI_RESP_DATA_PREFIX = 'data: '
-# OPENAI_RESP_DONE = '[DONE]'
-#
-# def openai_request(url, data, options):
-#     enable_auth=options['enable_auth']
-#     headers = {
-#         "Content-Type": "application/json",
-#     }
-#     if enable_auth:
-#         (OPENAI_API_KEY, OPENAI_ORG_ID) = load_api_key()
-#         headers['Authorization'] = f"Bearer {OPENAI_API_KEY}"
-#
-#         if OPENAI_ORG_ID is not None:
-#             headers["OpenAI-Organization"] =  f"{OPENAI_ORG_ID}"
-#
-#     request_timeout=options['request_timeout']
-#     req = urllib.request.Request(
-#         url,
-#         data=json.dumps({ **data }).encode("utf-8"),
-#         headers=headers,
-#         method="POST",
-#     )
-#     with urllib.request.urlopen(req, timeout=request_timeout) as response:
-#         for line_bytes in response:
-#             line = line_bytes.decode("utf-8", errors="replace")
-#             if line.startswith(OPENAI_RESP_DATA_PREFIX):
-#                 line_data = line[len(OPENAI_RESP_DATA_PREFIX):-1]
-#                 if line_data.strip() == OPENAI_RESP_DONE:
-#                     pass
-#                 else:
-#                     openai_obj = json.loads(line_data)
-#                     yield openai_obj
+class Config:
 
-# def enhance_roles_with_custom_function(roles):
-#     if vim.eval("exists('g:vim_ai_roles_config_function')") == '1':
-#         roles_config_function = vim.eval("g:vim_ai_roles_config_function")
-#         if not vim.eval("exists('*" + roles_config_function + "')"):
-#             raise Exception(f"Role config function does not exist: {roles_config_function}")
-#         else:
-#             roles.update(vim.eval(roles_config_function + "()"))
-#
-# def load_role_config(role):
-#     roles_config_path = os.path.expanduser(vim.eval("g:vim_ai_roles_config_file"))
-#     if not os.path.exists(roles_config_path):
-#         raise Exception(f"Role config file does not exist: {roles_config_path}")
-#
-#     roles = configparser.ConfigParser()
-#     roles.read(roles_config_path)
-#
-#     enhance_roles_with_custom_function(roles)
-#
-#     if not role in roles:
-#         raise Exception(f"Role `{role}` not found")
-#
-#     options = roles[f"{role}.options"] if f"{role}.options" in roles else {}
-#     options_complete =roles[f"{role}.options-complete"] if f"{role}.options-complete" in roles else {}
-#     options_chat = roles[f"{role}.options-chat"] if f"{role}.options-chat" in roles else {}
-#
-#     return {
-#         'role': dict(roles[role]),
-#         'options': {
-#             'options_default': dict(options),
-#             'options_complete': dict(options_complete),
-#             'options_chat': dict(options_chat),
-#         },
-#     }
+    @property
+    def is_debugging(self):
+        return vim.eval("g:vim_ai_debug") == "1"
 
-# empty_role_options = {
-#     'options_default': {},
-#     'options_complete': {},
-#     'options_chat': {},
-# }
+    @property
+    def debug_log_file(self):
+        return vim.eval("g:vim_ai_debug_log_file")
 
-# def parse_prompt_and_role(raw_prompt):
-#     prompt = raw_prompt.strip()
-#     role = re.split(' |:', prompt)[0]
-#     if not role.startswith('/'):
-#         # does not require role
-#         return (prompt, empty_role_options)
-#
-#     prompt = prompt[len(role):].strip()
-#     role = role[1:]
-#
-#     config = load_role_config(role)
-#     if 'prompt' in config['role'] and config['role']['prompt']:
-#         delim = '' if prompt.startswith(':') else ':\n'
-#         prompt = config['role']['prompt'] + delim + prompt
-#     return (prompt, config['options'])
+    @property
+    def pwd(self):
+        return vim.eval("getcwd()")
+
+    @property
+    def request_timeout(self):
+        return 30
+
+
+config = Config()
+
+
+class LolmaxBackend:
+
+    def __init__(self,
+                 root_url="http://localhost:8000",
+                 request_timeout=config.request_timeout):
+        self.root_url = root_url
+        self.request_timeout = request_timeout
+
+    def chat(self, messages, model='perplexity', effects=None):
+        effects = effects or []
+        url = os.path.join(self.root_url, "chat")
+        headers = {
+            "Content-Type": "application/json",
+        }
+        data = dict(
+            model=model,
+            effects=effects,
+            messages=messages,
+        )
+        req = urllib.request.Request(
+            url,
+            data=json.dumps(data).encode("utf-8"),
+            headers=headers,
+            method="POST",
+        )
+        with urllib.request.urlopen(req,
+                                    timeout=self.request_timeout) as response:
+            for line_bytes in response:
+                line = line_bytes.decode("utf-8", errors="replace")
+                line_obj = json.loads(line)
+                yield line_obj["content"]
+
+
+# chat and completion are very similar now, but tantalizingly difficult to fully DRY up with
+# the interleavings of common and bespoke functionality. Leaving as-is for now, since
+# It's Fine (tm)
+def do_chat():
+    prompt, role = parse_prompt_and_role(vim.eval("l:prompt"))
+    initialize_chat_window()
+    chat_content = vim.eval('trim(join(getline(1, "$"), "\n"))')
+    messages = parse_chat_messages(chat_content)
+
+    try:
+        if messages[-1]["content"].strip():
+            vim.command("normal! Go\n<<< assistant\n\n")
+            vim.command("redraw")
+
+            print(f'Answering...')
+            vim.command("redraw")
+
+            printDebug("[chat] Answering with args: {}", sys.argv)
+            printDebug("[chat] messages: {}", messages)
+            backend = LolmaxBackend()
+            text_chunks = backend.chat(messages)
+            is_selection = vim.eval("l:is_selection")
+            render_text_chunks(text_chunks, is_selection)
+
+            vim.command("normal! a\n\n>>> user\n\n")
+            vim.command("redraw")
+            clear_echo_message()
+    except BaseException as error:
+        handle_completion_error(error)
+        printDebug("[chat] error: {}", traceback.format_exc())
+
+
+def do_complete():
+    prompt, role = parse_prompt_and_role(vim.eval("l:prompt"))
+    messages = [{"role": "user", "content": prompt}]
+    try:
+        if messages[-1]["content"].strip():
+            print('Completing...')
+            vim.command("redraw")
+
+            backend = LolmaxBackend()
+            text_chunks = backend.chat(messages)
+            is_selection = vim.eval("l:is_selection")
+            render_text_chunks(text_chunks, is_selection)
+
+            clear_echo_message()
+    except BaseException as error:
+        handle_completion_error(error)
+        printDebug("[complete] error: {}", traceback.format_exc())
